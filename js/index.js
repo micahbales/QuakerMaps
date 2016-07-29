d4869b5c349b866a6967d36893912adfe8ae3280
Micah Bales <micahbales@gmail.com>
2016-07-28 21:47:08 -0400

    added more data points in json, removed comments

--- js/index.js
+++ js/index.js
@@ -9,20 +9,27 @@ function initMap() {
       zoom: 11
     });
 
-  $.getJSON('js/meetings.json', function (meeting) {
-    console.log(meeting['meetings'][0]['address']);
+  $.getJSON('js/meetings.json', (meeting) => {
 
-    $.each(meeting.meetings, function (key, data) {
-      console.log('key : ' + key + ' & data : ' + data.name );
+    $.each(meeting.meetings, (key, data) => {
+
       var mtgAddress = meeting['meetings'][key]['address'].split(' ').join('+');
-      console.log(mtgAddress);
-      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + mtgAddress + '&key=AIzaSyAZInx_2Z-qyDZXz2eMhSCd9xBLyeiAf7Q', function (locationData) {
-        console.log(locationData.results[0].geometry.location);
+
+      $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=' + mtgAddress + '&key=AIzaSyAZInx_2Z-qyDZXz2eMhSCd9xBLyeiAf7Q', (locationData) => {
+
         var geolocation = locationData.results[0].geometry.location;
         var lat = locationData.results[0].geometry.location.lat;
         var lng = locationData.results[0].geometry.location.lng;
-        var meetingAddress = meeting.meetings[key].address;
         var meetingName = meeting.meetings[key].name;
+        var meetingAddress = meeting.meetings[key].address;
+        var meetingPhone = meeting.meetings[key].phone;
+        var meetingWebsite = meeting.meetings[key].website;
+        var meetingEmail = meeting.meetings[key].email;
+        var meetingYM = meeting.meetings[key].ym;
+        var meetingTime = meeting.meetings[key].time;
+        var meetingStyle = meeting.meetings[key].style;
+        var meetingBranch = meeting.meetings[key].branch;
+        var windowContent = meetingName + "<br />" + meetingAddress + "<br />" + meetingPhone + "<br />" + meetingWebsite + "<br />" + meetingEmail + "<br />" + meetingTime;
 
         var marker = new google.maps.Marker({
           position: geolocation,
@@ -33,11 +40,11 @@ function initMap() {
         });
 
         var infoWindow = new google.maps.InfoWindow({
-              content: meetingName
+              content: windowContent
 
             });
 
-        google.maps.event.addListener(marker, 'click', function () {
+        google.maps.event.addListener(marker, 'click', () => {
             infoWindow.open(map, marker);
             });
       });
